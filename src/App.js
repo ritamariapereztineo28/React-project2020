a80e6e2129cabae5bc7e8fad4fdb6abd0718c436
ritamariapereztineo28 <riperez@intellisys.com.do>
2020-02-15 20:00:56 -0400

    Agregando Componente (Search), de igual forma se ha agregado Input y Boton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import Search from './components/Search'
 import './App.css';
 
 function App() {
   return (
-   <div>Hello World !</div>
+   <div>
+   <Search/>
+   </div>
     );
 }
 
