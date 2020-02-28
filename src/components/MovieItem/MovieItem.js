b8312682d548f37da52ec9234832c08193843dba
ritamariapereztineo28 <riperez@intellisys.com.do>
2020-02-28 15:14:19 -0400

    correccion de estilos

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 import "./MovieItem.css";
 
 class MovieItem extends Component {
   render() {
     return (
       <div className="container-movieItem">
-        <h1 className="title">{this.props.title}</h1>
+        <h1 className="title-movie">{this.props.title}</h1>
         <img src={this.props.imgUrl} alt="imagen" />
         <h2 className="year">{this.props.year}</h2>
       </div>
     );
   }
+}
+
+MovieItem.propTypes = {
+  title : PropTypes.string,
+  year: PropTypes.number
 }
 
 export default MovieItem;
