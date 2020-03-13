236ed5c4a712adcb418937b4c927d5c4a857a9af
ritamariapereztineo28 <riperez@intellisys.com.do>
2020-03-13 16:02:35 -0400

    mas correcciones

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import App from './App';
-import renderer from 'react-test-renderer';
+import React from "react";
+import App from "./App";
+import renderer from "react-test-renderer";
 
-test('renders learn react link', () => {
-  const app = renderer.create(<App/>)
-  expect(app)
-
+test("renders learn react link", () => {
+  const app = renderer.create(<App />);
+  expect(app);
 });
